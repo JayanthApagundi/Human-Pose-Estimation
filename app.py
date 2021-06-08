dc45c94561f59ea885a28382040aae768039df25
Jayanth Apagundi <42775370+JayanthApagundi@users.noreply.github.com>
2021-06-08 16:34:08 +0530

    image thing

--- app.py
+++ app.py
@@ -1,29 +1,45 @@
+from flask import Flask,abort,render_template,request,redirect,url_for,flash
 import os
-from flask import Flask,abort,render_template,request,redirect,url_for
+import urllib.request
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = './upload'
-ALLOWED_EXTENSIONS = set(['txt','jpg','png','jpeg'])
-
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'upload/'
+
+app.secret_key = "Human pose estimation"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+ALLOWED_EXTENSIONS = set(['jpg','png','jpeg'])
+
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST':
-        file = request.files['file']
-        file.save(os.path.join("upload", file.filename))
-        return render_template('check.html')
-    return render_template('check.html')
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect('/')
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('Image successfully uploaded and displayed below')
+        return render_template('check.html', filename=filename)
+    else:
+        flash('Allowed image types are - png, jpg, jpeg')
+        return redirect('/')
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('templates/check', filename='upload/' + filename))
 
 
 if __name__ == "__main__":
