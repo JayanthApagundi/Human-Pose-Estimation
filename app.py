5943e30ee24ce27fc8cd223229f2680fd5feb4fc
Humaid Desai <43137815+Humaid-0501@users.noreply.github.com>
2021-06-08 14:00:06 +0530

    Upload Success

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template, url_for, request
+import os
+from flask import Flask,abort,render_template,request,redirect,url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './upload'
+ALLOWED_EXTENSIONS = set(['txt','jpg','png','jpeg'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save(os.path.join("upload", file.filename))
+        return render_template('check.html')
+    return render_template('check.html')
 
 
 if __name__ == "__main__":
