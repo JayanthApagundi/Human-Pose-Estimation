d1fcf44db91573da1da70d5dd90e21c89e9cc31b
Jayanth Apagundi <42775370+JayanthApagundi@users.noreply.github.com>
2021-06-13 15:40:59 +0530

    Refactoring done

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask,abort,render_template,request,redirect,url_for,flash, Response
+from flask import Flask, render_template, request, redirect, url_for, Response, flash
 import os
-import urllib.request
 from werkzeug.utils import secure_filename
 import cv2
 import mediapipe as mp
-import time
 
 app = Flask(__name__)
 
@@ -22,7 +20,6 @@ mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 
-
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
@@ -33,16 +30,15 @@ def index():
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
     if 'file' not in request.files:
-        # flash('No file part')
+        flash('No file part', 'danger')
         return redirect('/')
     file = request.files['file']
     if file.filename == '':
-        # flash('No image selected for uploading')
+        flash('No image selected for uploading', 'danger')
         return redirect('/')
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # flash('Image successfully uploaded and displayed below')
         process_file(os.path.join(UPLOAD_FOLDER, filename), filename)
         data = {
             "processed_img": 'static/download/' + filename,
@@ -50,7 +46,7 @@ def upload():
         }
         return render_template('image.html', data=data)
     else:
-        # flash('Allowed image types are - png, jpg, jpeg')
+        flash('Allowed image types are - png, jpg, jpeg', 'danger')
         return redirect('/')
 
 def process_file(path, filename):
@@ -63,7 +59,6 @@ def display_image(path, filename):
             model_complexity=1,
             min_detection_confidence=0.5) as pose:
         image = cv2.imread(path)
-        # image_height, image_width, _ = image.shape
         results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
         # print(results.pose_landmarks)
         if results.pose_landmarks:
@@ -78,103 +73,58 @@ def display_image(path, filename):
 @app.route('/vidupload', methods=['GET', 'POST'])
 def vidupload():
     if 'file' not in request.files:
-        #flash('No file part')
+        flash('No file part', 'danger')
         return redirect('/')
     file = request.files['file']
     if file.filename == '':
-        #flash('No image selected for uploading')
+        flash('No video selected for uploading', 'danger')
         return redirect('/')
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # print('upload_image filename: ' + filename)
-        #flash('Image successfully uploaded and displayed below')
-        ################
-        # process_vidfile(os.path.join(UPLOAD_FOLDER, filename), filename)
-        # time.sleep(18)
-        # data = {
-        #     "processed_vid": 'static/download/' + filename,
-        #     "uploaded_vid": 'static/upload/' + filename
-        # }
-
-        ################
-        # return render_template('video.html', data=data)
         return Response(displayvid(os.path.join(UPLOAD_FOLDER, filename)), mimetype='multipart/x-mixed-replace; boundary=frame')
     else:
-        #flash('Allowed image types are - png, jpg, jpeg, gif')
+        flash('Allowed video type - mp4', 'danger')
         return redirect('/')
-
-# def process_vidfile(path, filename):
-#     displayvid(path, filename)
 
 @app.route('/displayvid/<filename>', methods=['GET', 'POST'])
 def displayvid(path):
-    ####### MODEL############
-    cap = cv2.VideoCapture(path)  # for webcam use '0'
-    # pTime = 0
-
-    # dir = r'static/download'
-    # os.chdir(dir)
-    #
-    # frame_width = int(cap.get(3))
-    # frame_height = int(cap.get(4))
-    #
-    # size = (frame_width, frame_height)
-    # result = cv2.VideoWriter(filename,
-    #                          cv2.VideoWriter_fourcc(*'mp4v'),
-    #                          20, size) # *'MP4V'
-    # vid_array=[]
+    cap = cv2.VideoCapture(path)
     while True:
-        success, img = cap.read()  # img is in BGR but mediapipe uses RGB
+        success, img = cap.read()
         if success == False:
             break
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # converts BGR to RGB
-        results = pose.process(imgRGB)  # sending the image to our model
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = pose.process(imgRGB)
 
         if results.pose_landmarks:
-            mpDraw.draw_landmarks(img, results.pose_landmarks,mpPose.POSE_CONNECTIONS)  # to draw keypoints(pose_landmarks) and connections(pose_CONNECTION)
+            mpDraw.draw_landmarks(img, results.pose_landmarks,mpPose.POSE_CONNECTIONS)
             for id, lm in enumerate(results.pose_landmarks.landmark):
                 h, w, c = img.shape
                 # print(id, lm)
-                cx, cy = int(lm.x * w), int(lm.y * h)  # to get pixel value of x,y landmarks
+                cx, cy = int(lm.x * w), int(lm.y * h)
                 cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
 
         ret, buffer = cv2.imencode('.jpg', img)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        # cTime = time.time()
-        # fps = 1 / (cTime - pTime)
-        # pTime = cTime
-        #
-        # cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
-        #             (255, 0, 0), 3)
-        # vid_array.append(img)
-        # if success == True:
-        #     result.write(img)
-        # cv2.imshow("Image", img)
-        # cv2.waitKey(1)
-
-    # return redirect(url_for('static', filename='upload/' + filename), code=301)
-    # out = cv2.VideoWriter(filename,cv2.VideoWriter_fourcc(*'mp4v'), 20, size)
-    # for i in range(len(vid_array)):
-    #     out.write(vid_array[i])
 
 def live():
     cap = cv2.VideoCapture(0)
     while True:
-        success, img = cap.read()  # img is in BGR but mediapipe uses RGB
+        success, img = cap.read()
         if success == False:
             break
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # converts BGR to RGB
-        results = pose.process(imgRGB)  # sending the image to our model
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = pose.process(imgRGB)
 
         if results.pose_landmarks:
-            mpDraw.draw_landmarks(img, results.pose_landmarks,mpPose.POSE_CONNECTIONS)  # to draw keypoints(pose_landmarks) and connections(pose_CONNECTION)
+            mpDraw.draw_landmarks(img, results.pose_landmarks,mpPose.POSE_CONNECTIONS)
             for id, lm in enumerate(results.pose_landmarks.landmark):
                 h, w, c = img.shape
                 # print(id, lm)
-                cx, cy = int(lm.x * w), int(lm.y * h)  # to get pixel value of x,y landmarks
+                cx, cy = int(lm.x * w), int(lm.y * h)
                 cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
 
         ret, buffer = cv2.imencode('.jpg', img)
@@ -182,14 +132,8 @@ def live():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-        # cv2.imshow("Image", img)
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #     break
-    # return render_template('live.html')
-
 @app.route('/video_feed', methods=['GET', 'POST'])
 def video_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
     return Response(live(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
